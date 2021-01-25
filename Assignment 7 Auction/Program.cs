9a70f55bd8b9adef118efcb1dfc1f1cbb8187576
prem <premplayz@gmail.com>
2021-01-25 19:38:53 +0700

    Set Bid and EndAuction

--- Assignment 7 Auction/Program.cs
+++ Assignment 7 Auction/Program.cs
@@ -12,12 +12,18 @@ namespace Assignment_7_Auction
             
             Items[1] = new Item("Inn's Gucci belt", 1000000);
 
-            Console.WriteLine(Items[0].GetItemDescription());
-            Items[0].SetHighestBid(50000,123456);
-            Console.WriteLine(Items[0].GetHighestBid());
-            Console.WriteLine(Items[0].GetBidderID());
+            //Console.WriteLine(Items[0].GetItemDescription());
+            //Items[0].SetHighestBid(50000,123456);
+            //Console.WriteLine(Items[0].GetHighestBid());
+            //Console.WriteLine(Items[0].GetBidderID());
 
             PrintAllItems(Items);
+
+            SetBid(Items, 0, 50000, 123123);
+
+            PrintAllItems(Items);
+
+            EndAuction(Items);
 
             static void PrintAllItems(Item[] Items)
             {
@@ -26,6 +32,27 @@ namespace Assignment_7_Auction
                     if (Items[i] != null)
                     {
                         Console.WriteLine(Items[i].GetInfo());
+                    }
+                }
+            }
+            static void SetBid(Item[] Items, int ItemNumber, double bid, int ID)
+            {
+                if (bid > Items[ItemNumber].GetHighestBid())
+                {
+                    Items[ItemNumber].SetHighestBid(bid, ID);
+                }
+            }
+            static void EndAuction(Item[] Items)
+            {
+                for (int i = 0; i < Items.Length; i += 1)
+                {
+                    if (Items[i] != null)
+                    {
+                        if (Items[i].GetHighestBid() != Items[i].GetReservePrice())
+                        {
+                            Console.WriteLine("Item:" + Items[i].GetItemDescription() + "   Sold for:" + Items[i].GetHighestBid()
+                                + "   To:" + Items[i].GetBidderID());
+                        }
                     }
                 }
             }
