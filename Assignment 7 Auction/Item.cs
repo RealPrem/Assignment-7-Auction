9fbc2bc82d3474a6781ec68af4ca149cc9c4bd7e
prem <premplayz@gmail.com>
2021-01-25 12:26:02 +0700

    Add project files.

---
+++ Assignment 7 Auction/Item.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_7_Auction
+{
+    class Item
+    {
+        private string ItemDescription;
+        private double ReservePrice;
+        private double HighestBid;
+        private int BidderID;
+
+        public Item(string ItemDescription, double ReservePrice)
+        {
+            this.ItemDescription = ItemDescription;
+            this.ReservePrice = ReservePrice;
+            HighestBid = ReservePrice;
+        }
+        public string GetItemDescription()
+        {
+            return ItemDescription;
+        }
+        public double GetReservePrice()
+        {
+            return ReservePrice;
+        }
+        public bool SetHighestBid(double Bid, int ID)
+        {
+            if (Bid > HighestBid)
+            {
+                HighestBid = Bid;
+                SetBidderID(ID);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Sorry, Bid not high enough");
+                return false;
+            }
+        }
+        public double GetHighestBid()
+        {
+            return HighestBid;
+        }
+        public void SetBidderID(int ID)
+        {
+            BidderID = ID;
+        }
+        public int GetBidderID()
+        {
+            return BidderID;
+        }
+        public string GetInfo()
+        {
+            return ItemDescription + " " + HighestBid + " " + ReservePrice;
+        }
+    }
+
+}
---
+++ Assignment 7 Auction/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Assignment_7_Auction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Item[] Items = new Item[5];
+            
+            Items[0] = new Item("Exotic Egg", 10000);
+            
+            Items[1] = new Item("Inn's Gucci belt", 1000000);
+
+            Console.WriteLine(Items[0].GetItemDescription());
+            Items[0].SetHighestBid(50000,123456);
+            Console.WriteLine(Items[0].GetHighestBid());
+            Console.WriteLine(Items[0].GetBidderID());
+            
+            static void PrintAllItems(Item[] Items)
+            {
+                for (int i = 0; i < Items.Length; i += 1)
+                {
+                    if (Items[i] != null)
+                    {
+                        Console.WriteLine(Items[i].GetInfo());
+                    }
+                }
+            }
+        }
+    }
+}
